fdf3b1053e5d505d2eebc0d27b28c407764b9219
PecasDev <juangc1004@hotmail.com>
2025-01-23 12:16:13 +0100

    Fixed Merge Conflicts
    Fix of Merge Conflicts of EntityAttributes.cs and fixed deleted components in Scene.

--- Assets/Scripts/Characters/Entities/EntityAttributes.cs
+++ Assets/Scripts/Characters/Entities/EntityAttributes.cs
@@ -10,26 +10,6 @@ public class EntityAttributes : MonoBehaviour
     [Tooltip("Controls the Rotation Speed (Degrees per second).")]
     public float rotationSpeedPlayer = 720f;
 
-
-    [Header("Health Variables")]
-    [Tooltip("Max Health of the Entity")]
-    public int maxHealth;
-    public int health;
-
-
-    void Start()
-    {
-        health = maxHealth;
-    }
-
-    void Update()
-    {
-        if (health <= 0) // Here can be stuff that is universal for all entities
-        {
-            //Destroy(this.gameObject);
-        }
-    }
-
     public Vector3 GetFacingDirection()
     {
         return transform.forward;
