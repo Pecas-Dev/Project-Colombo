8b2e9dc66872c5c0f2f4c070301f3f07a265df72
JulianLet <julian.letsche.7770@student.uu.se>
2025-04-14 11:53:06 +0200

    emil scene

---
+++ Assets/Scripts/World/TileLayout/Buildings/ModularHouseRandomizer.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectColombo.LevelManagement
+{
+    public class ModularHouseRandomizer : MonoBehaviour
+    {
+        public List<GameObject> baseModuls;
+        public float baseModuleHeight = 5;
+        public List<GameObject> firstLayerModuls;
+        public float firstLayerHeight = 5;
+        public List<GameObject> secondLayerModuls;
+        public float secondLayerHeight = 5;
+
+
+    }
+}
