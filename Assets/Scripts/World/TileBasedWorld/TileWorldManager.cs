2836278a81ac6f408ebe36510ced9a49791b9a1e
JulianLet <julian.letsche.7770@student.uu.se>
2025-05-05 16:44:12 +0200

    updated shortstacks

--- Assets/Scripts/World/TileBasedWorld/TileWorldManager.cs
+++ Assets/Scripts/World/TileBasedWorld/TileWorldManager.cs
@@ -144,7 +144,7 @@ namespace ProjectColombo.LevelManagement
         int worldHeight = 25;
         int worldWidth = 0;
         int chamberOffsetX = 3;
-        int chamberOffsetY = 2;
+        int chamberOffsetY = 3;
 
         public List<GameObject> chamberVariants;
         public List<GameObject> ICorridors;
