e3a3257101960014a41a1024d0be77b4c02e56d9
JulianLet <julian.letsche.7770@student.uu.se>
2025-05-05 14:37:06 +0200

    shortstack building can be flipped

--- Assets/Scripts/World/TileLayout/Buildings/HouseRandomizer.cs
+++ Assets/Scripts/World/TileLayout/Buildings/HouseRandomizer.cs
@@ -7,12 +7,21 @@ namespace ProjectColombo.LevelManagement
     public class HouseRandomizer : MonoBehaviour
     {
         public List<GameObject> buildingVariants;
-
+        public float chanceOfInvert = 0.5f;
         private void Start()
         {
             int rand = Random.Range(0, buildingVariants.Count);
+            float invert = Random.Range(0f, 1f);
+            Vector3 scale = transform.parent.localScale;
+
+            if (invert < chanceOfInvert)
+            {
+                scale.y *= -1;
+            }
 
             Instantiate(buildingVariants[rand], transform.position, transform.rotation, transform.parent);
+            transform.parent.localScale = scale;
+
             Destroy(this.gameObject);
         }
     }
