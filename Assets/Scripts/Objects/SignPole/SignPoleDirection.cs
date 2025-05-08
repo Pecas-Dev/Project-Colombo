026ba044cbb813d876fb49bee209b33b091d1992
JulianLet <julian.letsche.7770@student.uu.se>
2025-05-08 13:52:40 +0200

    added signs fixed grass

---
+++ Assets/Scripts/Objects/SignPole/SignPoleDirection.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace ProjectColombo.Objects
+{
+    public class SignPoleDirection : MonoBehaviour
+    {
+        public float rotation = 15f;
+
+        private void Start()
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+            transform.rotation = Quaternion.FromToRotation(transform.right, Vector3.right) * transform.rotation;
+            transform.rotation *= Quaternion.Euler(0, rotation, 0);
+        }
+    }
+}
