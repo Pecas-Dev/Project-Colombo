891f4ac0b954c614f5f26ad735d90c619c4aa6c1
cris03sora <cris03sora@hotmail.com>
2025-05-12 14:39:18 +0200

    New prefab and music script
    Prefab for music manager and script to handle all songs and scenes

--- Assets/Scripts/Sound/Cris/MusicChanger.cs
+++ Assets/Scripts/Sound/Cris/MusicChanger.cs
@@ -1,66 +1,81 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 using ProjectColombo.GameManagement.Events;
 
-public class MusicChanger : MonoBehaviour
+public class AudioManager : MonoBehaviour
 {
-    public static MusicChanger Instance;
+    public static AudioManager Instance;
 
-    public AudioSource explorationMusic;
-    public AudioSource[] battleMusicLayers; // 4 variations of Battle Theme
+    [Header("General Music Settings")]
+    public float fadeSpeed = 1f;
 
-    [Range(0f, 1f)] public float musicIntensity = 0f; // Controls battle mix
-    [Range(0f, 1f)] public float battleBlend = 0f; // Blends the musics: 0 = exploration, 1 = battle
+    [Header("Menu Music")]
+    public AudioClip menuMusicClip;
 
-    public float fadeSpeed = 1f; // Fade speed for transitions
+    [Header("Exploration Sets")]
+    public AudioClip levelExplorationClip;
+    public AudioClip[] levelBattleClips;
+
+    [Header("Church Music Set")]
+    public AudioClip churchExplorationClip;
+    public AudioClip[] churchBattleClips;
+
+    private AudioSource explorationMusic;
+    private AudioSource[] battleMusicLayers;
+
+    private float musicIntensity = 0f;
+    private float battleBlend = 0f;
+
+    private string currentScene = "";
+
+    private bool isInGameplay = false;
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        explorationMusic = gameObject.AddComponent<AudioSource>();
+        explorationMusic.loop = true;
+
+        battleMusicLayers = new AudioSource[4];
+        for (int i = 0; i < 4; i++)
+        {
+            battleMusicLayers[i] = gameObject.AddComponent<AudioSource>();
+            battleMusicLayers[i].loop = true;
+            battleMusicLayers[i].volume = 0f;
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        explorationMusic.volume = 1f;
-        explorationMusic.Play();
-
-        foreach (var layer in battleMusicLayers)
-        {
-            layer.volume = 0f;
-            layer.Play();
-        }
-
-        battleBlend = 0f;
-        musicIntensity = 0f;
-
-        // Subscribe to chamber events
         CustomEvents.OnChamberActivated += HandleChamberActivated;
         CustomEvents.OnChamberFinished += HandleChamberFinished;
     }
 
     private void OnDestroy()
     {
-        // Unsubscribe to avoid memory leaks
         CustomEvents.OnChamberActivated -= HandleChamberActivated;
         CustomEvents.OnChamberFinished -= HandleChamberFinished;
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Fade exploration music out/in based on battleBlend
+        if (!isInGameplay) return;
+
         float targetExplorationVolume = 1f - battleBlend;
         explorationMusic.volume = Mathf.Lerp(explorationMusic.volume, targetExplorationVolume, Time.deltaTime * fadeSpeed);
 
-        // Fade in battle layers based on intensity and blend
         for (int i = 0; i < battleMusicLayers.Length; i++)
         {
             float intensityPerLayer = Mathf.Clamp01(musicIntensity - i * 0.25f);
@@ -69,17 +84,99 @@ public class MusicChanger : MonoBehaviour
         }
     }
 
-    // Call this to enter battle mode
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        currentScene = scene.name;
+
+        StopAllCoroutines();
+
+        switch (currentScene)
+        {
+            case "00_MainMenu":
+            case "01_MaskSelection":
+                PlayMenuMusic();
+                isInGameplay = false;
+                break;
+
+            case "02_LevelOne":
+            case "03_LevelTwo":
+            case "04_LevelThree":
+                PlayGameplayMusic(levelExplorationClip, levelBattleClips);
+                isInGameplay = true;
+                break;
+
+            case "05_Church":
+                PlayGameplayMusic(churchExplorationClip, churchBattleClips);
+                isInGameplay = true;
+                break;
+
+            case "06_WinScene":
+            case "07_LooseScene":
+                StopAllMusic();
+                isInGameplay = false;
+                break;
+
+            default:
+                StopAllMusic();
+                isInGameplay = false;
+                break;
+        }
+
+        battleBlend = 0f;
+        musicIntensity = 0f;
+    }
+
+    private void PlayMenuMusic()
+    {
+        StopAllMusic();
+
+        if (menuMusicClip == null) return;
+
+        explorationMusic.clip = menuMusicClip;
+        explorationMusic.volume = 1f;
+        explorationMusic.Play();
+    }
+
+    private void PlayGameplayMusic(AudioClip explorationClip, AudioClip[] battleClips)
+    {
+        StopAllMusic();
+
+        if (explorationClip == null || battleClips.Length < 4) return;
+
+        explorationMusic.clip = explorationClip;
+        explorationMusic.volume = 1f;
+        explorationMusic.Play();
+
+        for (int i = 0; i < 4; i++)
+        {
+            battleMusicLayers[i].clip = battleClips[i];
+            battleMusicLayers[i].volume = 0f;
+            battleMusicLayers[i].Play();
+        }
+    }
+
+    private void StopAllMusic()
+    {
+        explorationMusic.Stop();
+        for (int i = 0; i < battleMusicLayers.Length; i++)
+        {
+            battleMusicLayers[i].Stop();
+        }
+    }
+
     private void HandleChamberActivated()
     {
-        // Start battle music transition
         battleBlend = 1f;
-        musicIntensity = 0.5f; // Optional default intensity
+        musicIntensity = 0.5f;
     }
 
     private void HandleChamberFinished()
     {
-        // Return to exploration music
         battleBlend = 0f;
+    }
+
+    public void SetBattleIntensity(float intensity)
+    {
+        musicIntensity = Mathf.Clamp01(intensity);
     }
 }
