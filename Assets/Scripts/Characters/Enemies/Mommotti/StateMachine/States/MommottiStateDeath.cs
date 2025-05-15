f36f508e905de5e78fa36694679c4b4c686d298e
JulianLet <julian.letsche.7770@student.uu.se>
2025-05-15 11:28:39 +0200

    mommotti fade out with death

--- Assets/Scripts/Characters/Enemies/Mommotti/StateMachine/States/MommottiStateDeath.cs
+++ Assets/Scripts/Characters/Enemies/Mommotti/StateMachine/States/MommottiStateDeath.cs
@@ -1,6 +1,4 @@
 using ProjectColombo.GameManagement.Events;
-using System.Collections;
-using System.Threading;
 using UnityEngine;
 
 namespace ProjectColombo.StateMachine.Mommotti
@@ -10,6 +8,10 @@ namespace ProjectColombo.StateMachine.Mommotti
         float timer;
         float fadeTime = 1f;
         GameGlobals.MusicScale causedScale;
+
+        Material fadeMaterial;
+        Color baseColor;
+        float startAlpha;
 
         public MommottiStateDeath(MommottiStateMachine stateMachine, GameGlobals.MusicScale scale) : base(stateMachine)
         {
@@ -21,55 +23,39 @@ namespace ProjectColombo.StateMachine.Mommotti
             stateMachine.myAnimator.SetTrigger("Death");
             CustomEvents.EnemyDied(causedScale, stateMachine.gameObject);
 
-            //// Create a new material instance using the URP shader
-            //Material materialInstance = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+            fadeMaterial = stateMachine.myFadeOutSkin.material;
 
-            //if (materialInstance == null)
-            //{
-            //    Debug.LogError("URP Shader not found! Make sure URP is properly installed.");
-            //    return;
-            //}
+            // Setup URP Lit material for transparency
+            fadeMaterial.SetFloat("_Surface", 1); // Transparent
+            fadeMaterial.SetFloat("_Blend", 0);   // Alpha blending
+            fadeMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            fadeMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            fadeMaterial.SetInt("_ZWrite", 0);
+            fadeMaterial.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
+            fadeMaterial.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
 
-            //// Assign the material instance to the renderer
-            //stateMachine.myMajorMinorSkin.material = materialInstance;
-
-            //// Ensure transparency works in URP
-            //materialInstance.SetFloat("_Surface", 1); // 1 = Transparent, 0 = Opaque
-            //materialInstance.SetFloat("_Blend", 0); // 0 = Alpha blending
-            //materialInstance.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-            //materialInstance.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            //materialInstance.SetInt("_ZWrite", 0); // Disable ZWrite for transparency
-            //materialInstance.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
-            //materialInstance.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent; // Set proper render order
+            baseColor = fadeMaterial.GetColor("_BaseColor");
+            startAlpha = baseColor.a;
 
             stateMachine.SetCurrentState(MommottiStateMachine.MommottiState.DEAD);
-            //Debug.Log("Mommotti entered Death State");
         }
 
         public override void Tick(float deltaTime)
         {
             timer += deltaTime;
 
-            if (timer > fadeTime)
+            // Fade out over fadeTime
+            if (timer <= fadeTime)
             {
+                float alpha = Mathf.Lerp(startAlpha, 0f, timer / fadeTime);
+                baseColor.a = alpha;
+                fadeMaterial.SetColor("_BaseColor", baseColor);
+            }
+            else
+            {
+                // Fully faded, destroy the entity
                 stateMachine.myEntityAttributes.Destroy();
             }
-
-            //Material materialInstance = stateMachine.myMajorMinorSkin.material;
-            //Color color = materialInstance.color;
-            //float alpha = color.a;
-
-            //alpha = Mathf.Max(0, alpha - deltaTime / fadeTime);
-
-            //if (alpha <= 0)
-            //{
-            //    stateMachine.myEntityAttributes.Destroy();
-            //}
-            //else
-            //{
-            //    color.a = alpha;
-            //    materialInstance.color = color;
-            //}
         }
 
         public override void Exit()
