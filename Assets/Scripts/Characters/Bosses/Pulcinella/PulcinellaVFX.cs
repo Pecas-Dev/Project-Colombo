6a776821c7b6f94b1685000bfbb16d3b23700c9f
JulianLet <julian.letsche.7770@student.uu.se>
2025-06-02 14:28:50 +0200

    pulcinella smoke vfx, pre church dialog, tutorial dialog portriat

--- Assets/Scripts/Characters/Bosses/Pulcinella/PulcinellaVFX.cs
+++ Assets/Scripts/Characters/Bosses/Pulcinella/PulcinellaVFX.cs
@@ -1,4 +1,5 @@
 using ProjectColombo.GameManagement.Events;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -8,6 +9,8 @@ namespace ProjectColombo.VFX
     {
         public VisualEffect ragefullImpactSlam;
         public VisualEffect cursedLeapLand;
+        public VisualEffect minorEffect;
+        public VisualEffect majorEffect;
 
 
         public void PlayRI_ImpactVFX()
@@ -18,6 +21,44 @@ namespace ProjectColombo.VFX
         public void PlayCL_LandVFX()
         {
             cursedLeapLand.Play();
+        }
+
+        public void PlayMajorMinorEffect()
+        {
+            GameGlobals.MusicScale scale = GetComponent<EntityAttributes>().currentScale;
+
+            if (scale == GameGlobals.MusicScale.MAJOR)
+            {
+                minorEffect.Stop();
+                majorEffect.Play();
+                StartCoroutine(AnimateSmokeEffekt(majorEffect));
+            }
+            else if (scale == GameGlobals.MusicScale.MINOR)
+            {
+                majorEffect.Stop();
+                minorEffect.Play();
+                StartCoroutine(AnimateSmokeEffekt(minorEffect));
+            }
+        }
+
+        public void OnDestroy()
+        {
+            StopAllCoroutines();
+        }
+
+        IEnumerator AnimateSmokeEffekt(VisualEffect effect)
+        {
+            int i = 0;
+
+            while (i < 15)
+            {
+                effect.SetFloat("SetSize", i);
+                i += 1;
+                yield return new WaitForEndOfFrame();
+            }
+
+            yield return new WaitForSeconds(1f);
+            effect.Stop();
         }
     }
 }
